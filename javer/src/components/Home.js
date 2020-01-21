e0a8e4b4b308f23cf648fc2aa4022b0e6a3ecf2f
vxda77 <vxda77@gmail.com>
2020-01-21 10:51:48 +0900

    center

--- javer/src/components/Home.js
+++ javer/src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-
 import { Link, Route } from "react-router-dom";
 import News from "./News";
 import { makeStyles } from "@material-ui/core/styles";
